86c2aa1e73bd26f1e642d44113bdb47c0409d2bb
Kristian Karabeliov <kristian@karabeliov.com>
2015-03-11 23:52:21 +0200

    Homeworks at Telerik Akademy 2015-2016.

---
+++ Homeworks/C#2/ExamCSharpPart2/1.Problem1/Problem1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+using System.Linq;
+
+class Problem1
+{
+    static int nBase = 19;
+    static void Main()
+    {
+        string[] words = Console.ReadLine().Split(' ');
+        List<int> numbersCat = new List<int>();
+        List<int> sumCatNumber = new List<int>();
+
+        int sum = 0;
+        foreach (var word in words)
+        {
+            for (int i = 0; i < word.Length; i++)
+            {
+                numbersCat.Add(word[i] - 'a');  
+            }
+
+            int decimalNumber = DecimalRepresentation(numbersCat);
+            
+            sum += decimalNumber;
+            decimalNumber = 0;
+            numbersCat.Clear();
+        }
+
+        string catNumber = CatRepresentation(sum);
+
+        Console.WriteLine("{0} = {1}", catNumber, sum);
+    }
+
+    private static string CatRepresentation(int sum)
+    {
+        string catNumber = null;
+        int rest;
+        while (sum > 0)
+        {
+            rest = sum % 19;
+
+            switch (rest)
+            {
+                case 0: catNumber += 'a';
+                    break;
+                case 1: catNumber += 'b';
+                    break;
+                case 2: catNumber += 'c';
+                    break;
+                case 3: catNumber += 'd';
+                    break;
+                case 4: catNumber += 'e';
+                    break;
+                case 5: catNumber += 'f';
+                    break;
+                case 6: catNumber += 'g';
+                    break;
+                case 7: catNumber += 'h';
+                    break;
+                case 8: catNumber += 'i';
+                    break;
+                case 9: catNumber += 'j';
+                    break;
+                case 10: catNumber += 'k';
+                    break;
+                case 11: catNumber += 'l';
+                    break;
+                case 12: catNumber += 'm';
+                    break;
+                case 13: catNumber += 'n';
+                    break;
+                case 14: catNumber += 'o';
+                    break;
+                case 15: catNumber += 'p';
+                    break;
+                case 16: catNumber += 'q';
+                    break;
+                case 17: catNumber += 'r';
+                    break;
+                case 18: catNumber += 's';
+                    break;
+                default: catNumber += rest.ToString();
+                    break;
+            }
+
+            sum /= 19;
+        }
+
+        char[] array = catNumber.ToCharArray();
+        Array.Reverse(array);
+        return string.Join("", array);
+    }
+
+    private static int DecimalRepresentation(List<int> numbersCat)
+    {
+        int power = 0;
+        int decimalNumber = 0; // uint
+        for (int index = numbersCat.Count - 1; index >= 0; index--)
+        {
+            int currentNumber = numbersCat[index];
+            decimalNumber += currentNumber * Power(nBase, power);
+
+            power++;
+        }
+
+        return decimalNumber;
+    }
+
+    private static int Power(int currentNumber, int power)
+    {
+        int result = 1;
+        for (int i = 0; i < power; i++)
+        {
+            result *= currentNumber;
+        }
+
+        return result;
+    }
+}
---
+++ Homeworks/C#2/ExamCSharpPart2/2.Problem2/Problem2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+class Problem2
+{
+    static List<long> numbers = new List<long>();
+    static void Main()
+    {
+        // Input
+        numbers = Input();
+        BigInteger sumResult = 0;
+        
+        for (int i = 1; i < numbers.Count; i++)
+        {
+            BigInteger diff = Math.Abs(numbers[i - 1] - numbers[i]);
+
+            if (diff % 2 == 0 && i + 1 < numbers.Count)
+            {
+                i += 1;
+            }
+
+            if (diff % 2 != 0)
+            {
+                sumResult += diff;
+            }
+        }
+
+        // Output
+        Console.WriteLine(sumResult);
+    }
+
+    private static List<long> Input()
+    {
+        string[] input = Console.ReadLine().Split(' ');
+
+        numbers = new List<long>();
+
+        foreach (var n in input)
+        {
+            numbers.Add(int.Parse(n));
+        }
+
+        return numbers;
+    }
+}
