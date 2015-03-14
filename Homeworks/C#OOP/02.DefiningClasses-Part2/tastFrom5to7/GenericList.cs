9c7786f15f4d4e781757bdcfe622372712dfe8b7
Kristian Karabeliov <kristian@karabeliov.com>
2015-03-14 19:46:59 +0200

    Defining Classes - Part 2

--- Homeworks/C#OOP/02.DefiningClasses-Part2/tastFrom5to7/GenericList.cs
+++ Homeworks/C#OOP/02.DefiningClasses-Part2/tastFrom5to7/GenericList.cs
@@ -7,6 +7,144 @@
 
     public class GenericList<T>
     {
+        private const int DEFAULT_SIZE = 16;
+        private int index = 0;
+        private T[] list;
+        
+        public GenericList()
+        {
+            this.list = new T[DEFAULT_SIZE];
+        }
 
+        public GenericList(int size)
+            : this()
+        {
+            if (size < 0)
+            {
+                throw new IndexOutOfRangeException("Size must be bigger then zero");
+            }
+
+            this.list = new T[size];
+        }
+
+        public void Add(T element)
+        {
+            if (index == list.Length)
+            {
+                Resize();
+            }
+
+            list[index] = element;
+            index++;
+        }
+
+        private void Resize()
+        {
+            int size = list.Length * 2;
+            T[] newList = new T[size];
+
+            for (int i = 0; i < list.Length; i++)
+            {
+                newList[i] = list[i];
+            }
+
+            list = newList;
+        }
+
+        public T ElementAt(int position)
+        {
+            if (position < 0 || position > list.Length - 1 )
+            {
+                throw new IndexOutOfRangeException("Invalid position");
+            }
+
+            return list[position];
+        }
+
+        public void RemoveAt(int position)
+        {
+            if (position < 0 || position > list.Length - 1)
+            {
+                throw new IndexOutOfRangeException("Invalid position");
+            }
+
+            list[position] = default(T);
+        }
+
+        public void InsertAt(int position, T value)
+        {
+            if (position < 0 || position > index)
+            {
+                throw new IndexOutOfRangeException("Invalid position");
+            }
+
+            list[position] = value;
+        }
+
+        public void Clean()
+        {
+            Array.Clear(list, 0, list.Length);
+        }
+
+        public int GetIndex(T value)
+        {
+            return list.ToList().IndexOf(value);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+
+            for (int i = 0; i < index; i++)
+            {
+                result.Append(list[i] + ", ");
+            }
+
+            return result.ToString().TrimEnd(',', ' ');
+        }
+
+        public int Capacity 
+        { 
+            get
+            {
+                return list.ToList().Capacity;
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return index;
+            }
+        }
+
+        public T Min() 
+        {
+            T[] newList = new T[index];
+
+            for (int i = 0; i < index; i++)
+            {
+                newList[i] = list[i];
+            }
+
+            T min = newList.Min();
+
+            return min;
+        }
+
+        internal T Max()
+        {
+            T[] newList = new T[index];
+
+            for (int i = 0; i < index; i++)
+            {
+                newList[i] = list[i];
+            }
+
+            T max = newList.Max();
+
+            return max;
+        }
     }
 }
--- Homeworks/C#OOP/02.DefiningClasses-Part2/tastFrom5to7/tastFrom5to7.cs
+++ Homeworks/C#OOP/02.DefiningClasses-Part2/tastFrom5to7/tastFrom5to7.cs
@@ -9,7 +9,34 @@
     {
         static void Main()
         {
+            GenericList<int> listInt = new GenericList<int>();
+            GenericList<string> listString = new GenericList<string>(60);
 
+            listInt.Add(5);
+            listInt.Add(6);
+            listInt.Add(7);
+            listInt.Add(8);
+
+            listString.Add("Generic");
+            listString.Add("List");
+            int takeElement = listInt.ElementAt(0);
+
+            // list.InsertAt(1, 5);
+            // list.RemoveAt(0);
+            // list.Clean();
+            // int index = list.GetIndex(5);
+            // int capacity = list.Capacity;
+            // int count = list.Count;
+
+            int min = listInt.Min();
+            int max = listInt.Max();
+
+            string minString = listString.Min();
+            string maxString = listString.Max();
+
+            Console.WriteLine(listInt);
+
+            Console.WriteLine(listString);
         }
     }
 }
