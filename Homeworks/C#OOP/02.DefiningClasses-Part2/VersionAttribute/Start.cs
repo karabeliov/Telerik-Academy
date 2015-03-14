b39d701c046744147045b1383db6cec5f88bd790
Kristian Karabeliov <kristian@karabeliov.com>
2015-03-14 23:43:18 +0200

    Defining Classes - Part 2

---
+++ Homeworks/C#OOP/02.DefiningClasses-Part2/VersionAttribute/Start.cs
@@ -0,0 +1,37 @@
+﻿namespace VersionAttribute
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Reflection;
+
+    [Author("Kristian"), Author("I. Kenov", version = 2.0)]
+    class Start
+    {
+        static void Main()
+        {
+            PrintAuthorInfo(typeof(Start));
+
+            Console.WriteLine();
+        }
+
+        private static void PrintAuthorInfo(Type t)
+        {
+            Console.WriteLine("Author information for {0}", t);
+
+            // Using reflection.
+            Attribute[] attrs = Attribute.GetCustomAttributes(t);  // Reflection. 
+
+            // Displaying output. 
+            foreach (Attribute attr in attrs)
+            {
+                if (attr is Author)
+                {
+                    Author a = (Author)attr;
+                    Console.WriteLine("   {0}, version {1:f}", a.GetName(), a.version);
+                }
+            }
+        }
+    }
+}
---
+++ Homeworks/C#OOP/02.DefiningClasses-Part2/VersionAttribute/VersionAttribute.cs
@@ -0,0 +1,25 @@
+﻿namespace VersionAttribute
+{
+    using System;
+
+    // Multiuse attribute.
+    [AttributeUsage(AttributeTargets.All, AllowMultiple = true)]  // Multiuse attribute.
+    public class Author : Attribute
+    {
+        string name;
+        public double version;
+
+        public Author(string name)
+        {
+            this.name = name;
+
+            // Default value.
+            version = 1.0;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+    }
+}
