196e99e00416266a4deb1651a7bc131b7e60998f
Kristian Karabeliov <kristian@karabeliov.com>
2015-03-16 23:25:05 +0200

    Extension Methods Delegates Lambda LINQ

---
+++ Homeworks/C#OOP/03.ExtensionMethodsDelegatesLambdaLINQ/01.StringBuilderSubstring/StringBuilder.cs
@@ -0,0 +1,25 @@
+﻿namespace StringBuilderSubstring
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public static class StringBuilder
+    {
+        //private string text;
+
+        //public static StringBuilder Substring(this int index, int length)
+        //{
+            
+        //}
+
+        //public StringBuilder(string input)
+        //{
+        //    this.text = input;
+        //}
+
+        
+        
+    }
+}
---
+++ Homeworks/C#OOP/03.ExtensionMethodsDelegatesLambdaLINQ/01.StringBuilderSubstring/StringSubstring.cs
@@ -0,0 +1,18 @@
+﻿namespace StringBuilderSubstring
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class StringSubstring
+    {
+        static void Main()
+        {
+            //StringBuilder sb = new StringBuilder("Only THIS will remain after the substring.");
+            //Console.WriteLine(sb);
+            //sb = sb.Substring(5, 4);
+            //Console.WriteLine(sb);
+        }
+    }
+}
