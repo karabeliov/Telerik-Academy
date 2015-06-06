d9b1ff9a05366060535799a287f473829a56f254
Kristian Karabeliov <kristian@karabeliov.com>
2015-06-06 21:52:39 +0300

    Browser console

---
+++ Homeworks/JavaScript Fundamentals/BrowserConsole/js/script.js
@@ -0,0 +1,13 @@
+taskName = "JavaScript";
+
+function Main(bufferElement) {
+// Input
+    var integerNumber = 6, // Change value to TEST
+        string = "JavaScript";
+// Solution   
+     for (var i = 0; i < length; i++) {
+
+    }
+// Output
+WriteLine("Null value: " + valueNull);
+}
