e63dc7da2438c9da9957015109943fc428016a00
Kristian Karabeliov <kristian@karabeliov.com>
2015-06-08 19:20:07 +0300

    Exam-Preparation

--- Homeworks/JavaScript Fundamentals/Exam-Preparation/TA-2012-2013-JavaScript-Exam/03.ClojureParser/js/script.js
+++ Homeworks/JavaScript Fundamentals/Exam-Preparation/TA-2012-2013-JavaScript-Exam/03.ClojureParser/js/script.js
@@ -1,13 +1,36 @@
-taskName = "JavaScript";
+taskName = "ClojureParser";
 
 function Main(bufferElement) {
 // Input
-    var integerNumber = 6, // Change value to TEST
-        string = "JavaScript";
+    var input = '(def func 10)' +
+    '(def newFunc (+  func 2))' +
+    ' (def sumFunc (+ func func newFunc 0 0 0))' +
+    '(* sumFunc 2)',
+    test1 = '(+ 1 2)';
+// Change value to TEST'
+        
 // Solution   
-     for (var i = 0; i < length; i++) {
+    function Solve(params) {
 
+        function trimLast(str) {
+            var i, len, result = '';
+            for (i = 0, len = str.length; i < len; i++) {
+                if (str[0] === '(' && i === 0) {
+                    continue;
+                }
+                else if (str[len - 1] === ')' && i === len - 1) {
+                    continue;
+                }
+
+                result += str[i];
+            }
+
+            return result;
+        }
+        result = sum;
+        return result;
     }
-// Output
-WriteLine("Null value: " + valueNull);
+    // Output
+
+    WriteLine("Result: " + Solve(test1));
 }
