1d6a597f989fd2ce114b56a2f06e5e02dcb80601
Kristian Karabeliov <kristian@karabeliov.com>
2015-06-18 23:45:21 +0300

    Upload new Homeworks

--- Homeworks/JavaScript-OOP/02.Closures-and-Scopes/tasks/task-1.js
+++ Homeworks/JavaScript-OOP/02.Closures-and-Scopes/tasks/task-1.js
@@ -11,7 +11,7 @@
 				*	This can be done by author, by category or all
 			*	List all categories
 				*	Categories are sorted by ID
-		*	Each book/catagory has a unique identifier (ID) that is a number greater than 1
+		*	Each book/catagory has a unique identifier (ID) that is a number greater than or equal to 1
 			*	When adding a book/category, the ID is generated automatically
 		*	Add validation everywhere, where possible
 			*	Book title and category name must be between 2 and 100 characters, including letters, digits and special characters ('!', ',', '.', etc)
@@ -21,29 +21,75 @@
 			*	If something is not valid - throw Error
 */
 function solve() {
-	var library = (function () {
-		
-		function listBooks() {
+    var library = (function () {
+        var books = [];
+        var categories = [];
 
-		}
+        function listBooks() {
+          
+            return books;
+        }
 
-		function addBook(book) {
-		}
+        function addBook(book) {
+            book.ID = books.length + 1;
+            var i, len;
 
-		function listCategories() {
+            if (book.isbn.length != 10 && book.isbn.length != 13) {
+                throw new Error('Invalid ISBN!');
+            }
 
-		}
+            if (book.author == '') {
+                throw new Error('Name Author is invalid!');
+            }
 
-		return {
-			books: {
-				list: listBooks,
-				add: addBook
-			},
-			categories: {
-				list: listCategories
-			}
-		};
-	} ());
-	return library;
+            for (i = 0, len = books.length; i < len; i += 1) {
+                if (book.title == books[i].title) {
+                    throw new Error('This book is exist!');
+                }
+
+                if (book.isbn == books[i].isbn) {
+                    throw new Error('This book is exist!');
+                }
+            }
+            
+            if (true) {
+
+            }
+            books.push(book);
+
+            return book;
+        }
+
+        function listCategories() {
+            var i, len;
+            for (i = 0, len = books.length; i < len; i += 1) {
+                if (categories.indexOf(books[i].category) > -1) {
+                    continue;
+                }
+                else {
+                    categories.push(books[i].category);
+                }
+
+                if (categories[i] == undefined) {
+                    categories.push('Book Category');
+                }
+            }
+
+            return categories;
+        }
+
+        return {
+            books: {
+                list: listBooks,
+                add: addBook
+            },
+            categories: {
+                list: listCategories
+            }
+        };
+    }());
+
+    return library;
 }
+
 module.exports = solve;
